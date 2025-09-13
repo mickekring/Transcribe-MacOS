f2c5808ae3cc2edc3b3732194cd97d7d5bc3957f
Micke Kring <jag@mickekring.se>
2025-09-13 12:59:44 +0200

    Initial Commit

---
+++ Transcribe/TranscribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscribeApp.swift
+//  Transcribe
+//
+//  Created by Micke Kring on 2025-09-13.
+//
+
+import SwiftUI
+
+@main
+struct TranscribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
